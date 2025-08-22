51015a1906ab914e4a989771271138c76e624a2a
Sebas <jesross0303.sr@gmail.com>
2025-08-22 10:14:16 -0500

    .env rail

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ app = Flask(__name__)
 app.secret_key = 'tu_clave_secreta'
 
 # SQL CONFIG---------------------------------------------------------
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'sebas_r'
-app.config['MYSQL_PASSWORD'] = 'lemonsharkswim'
-app.config['MYSQL_DB'] = 'flask_login'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', '')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'mct_proyecto')
+
 
 mysql = MySQL(app)
 bcrypt = Bcrypt(app)
